bf1b23f5ab07b808618554a0e7c6a35330dd1244
LeeHyungGeol <rjf1138@naver.com>
2020-07-28 17:25:37 +0900

    second commit

---
+++ common/mod1.py
@@ -0,0 +1,4 @@
+def gugu(a):
+    for i in range(1, 10):
+        print('{} x {} = {}'.format(a, i, a*i))
+        
---
+++ common/mod2.py
@@ -0,0 +1,6 @@
+def mysum(a):
+	result = 0
+	for i in range(1,a+1):
+		result += i
+	return result
+
